de89c322320e03c4f2eb899baab3959faa3728d4
Raj bhoir <bhoirraj872@gmail.com>
2024-11-25 22:47:41 +0530

    Adding size in product variant

--- products/views.py
+++ products/views.py
@@ -3,5 +3,16 @@ from products.models import Product
 # Create your views here.
 
 def get_product(request,slug):
-    obj = Product.objects.get(slug=slug) 
-    return render(request,'products/products.html',context={'obj':obj})
+    
+    obj = Product.objects.get(slug=slug)
+    context = {'obj' : obj}
+    if request.GET.get('size'):
+        size = request.GET.get('size')
+        
+        price = obj.get_product_price_by_size(size)
+        context['selected_size'] = size
+        context['updated_price'] = price
+        
+        print(price)
+    return render(request,'products/products.html',context= context)
+    
